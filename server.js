8244bf8e4ba6fda1f082e4213f758cc664f9dca8
roger <rogerluizhenhao@gmail.com>
2020-01-20 22:54:03 +0800

    add PORT env to serverjs

--- server.js
+++ server.js
@@ -90,6 +90,7 @@ app.put("/tools", function(request, response) {
   );
 });
 
-app.listen(3000, function() {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  const portNumber = process.env.PORT || 3000;
+  console.log("Server is running on: " + portNumber);
 });
